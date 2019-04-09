0f5605f1d50f6e9e6bfa2e97e707ade8ead90198
Asaf Benjaminov <asaf.benjaminov@gameffective.com>
2019-04-09 18:45:51 +0300

    Fix bug starting server

--- src/server/server.js
+++ src/server/server.js
@@ -3,12 +3,12 @@ const path = require("path");
 
 const app = express();
 
-console.log(__dirname + '../../dist/israel-elite-fitness')
+console.log(__dirname + 'dist/israel-elite-fitness')
 
 app.use(express.static('dist/israel-elite-fitness'));
 
 app.get('/*', (req,res) => {
     res.sendFile(path.join('dist/israel-elite-fitness/index.html'));
+});
 
-    app.listen(process.env.PORT || 60123);
-})
+app.listen(process.env.PORT || 60123);
