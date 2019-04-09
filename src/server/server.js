9772892e51236696ce15027309b46ac3e470b8d0
Asaf Benjaminov <asaf.benjaminov@gameffective.com>
2019-04-09 18:37:27 +0300

    port change

--- src/server/server.js
+++ src/server/server.js
@@ -10,5 +10,5 @@ app.use(express.static(__dirname + '../../dist/israel-elite-fitness'));
 app.get('/*', (req,res) => {
     res.sendFile(path.join(__dirname + '../../dist/israel-elite-fitness/index.html'));
 
-    app.listen(process.env.PORT || 8080);
+    app.listen(process.env.PORT || 60123);
 })
